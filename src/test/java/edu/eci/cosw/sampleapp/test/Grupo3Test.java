6eca0d51895a94d511e4420e923e6cebdafaa7e3
leonardoIDS <2098167@linux35.labinfo.is.escuelaing.edu.co>
2015-11-05 14:32:34 -0500

    Se documento las clases de equivalencia

---
+++ src/test/java/edu/eci/cosw/sampleapp/test/Grupo3Test.java
@@ -0,0 +1,51 @@
+package edu.eci.cosw.sampleapp.test;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author 2098167 Leonardo Castro Susa
+ * 
+ * 
+ *     <   CLASES DE EQUIVALENCIA >
+ *  
+ * <HU1  (3) Reporte de problemas>
+ *      - CH1A: Reporta problemas de manera correcta.
+ *      - CH1B: Reporta problemas faltando algun dato no exigido (Comentarios adicionales).
+ *      - CH1C: Reporta problemas faltando algun dato exigido (Descripcion, laboratorio, equipo etc.).
+ *      
+ * 
+ * <HU2  (13) Gestión de la bitácora>
+ *      - CH2A: Realiza una entrada a la bitacora correctamente sin faltar datos.
+ *      - CH2B: Realiza una entrada a la bitacora sobre una tarea que no esta   en el planeador como pendiente 
+ *      - CH2C: Realiza una entrada sobre una tarea sin reportar su estado (resuelto o avanzado)
+ *      - CH2D: Realiza una entrada sobre una faltando datos 
+ *      - CH2E: Realiza una entrada sobre un soporte academico faltando datos.
+ * 
+ * <HU3  (8) Reporte de monitores>
+ * 
+ *      - CH3A: Realiza un reporte de manera correcta.
+ *      - CH3B: Realiza un reporte con seleccion de intervalo de fechas.
+ *      - CH3C: Realiza un reporte con seleccion de fechas organizados por el top N de lenguajes.
+ *      - CH3D: Realiza un reporte con las tareas discriminadas
+ * 
+ * 
+ * 
+ */
+
+
+
+
+public class Grupo3Test {
+
+    
+    
+  @Test
+  public void sampleTest() {
+	//fail("No hay pruebas implementadas");	
+  }
+
+} 
