dec52a55920e114520d942e0b678924b1061dbf4
leonardoIDS <leonardo.castro@mail.escuelaing.edu.co>
2015-11-05 12:37:26 -0500

    Update AppTest.java

--- src/test/java/edu/eci/cosw/sampleapp/test/AppTest.java
+++ src/test/java/edu/eci/cosw/sampleapp/test/AppTest.java
@@ -1,15 +1,42 @@
 package edu.eci.cosw.sampleapp.test;
 
 import static org.junit.Assert.fail;
-
 import org.junit.Test;
+import edu.eci.pdsw.math.crappyfractions.Fraction;
+import edu.eci.pdsw.math.crappyfractions.InvalidFractionException;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class AppTest {
+/**<
+ * @author 2098167 Leonardo Castro Susa
+ * >
+ * 
+ *            CLASES DE EQUIVALENCIA 
+ *  
+ * HU1  (3) Reporte de problemas
+ * 		
+ * 
+ * HU2  (13) Gestión de la bitácora
+ * 
+ * HU3  (8) Reporte de monitores
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 
+
+
+
+public class Grupo3Test {
+
+    
+    
   @Test
   public void sampleTest() {
 	//fail("No hay pruebas implementadas");	
   }
 
 } 
-
